349500c1fd76c4b30e7fbedd10d800ebae47ed7d
sahilprajapati2806 <sahilprajapati2806@gmail.com>
2026-03-02 11:53:32 +0530

    Updated project design and fixed bugs

--- src/Components/Pages/Auth.jsx
+++ src/Components/Pages/Auth.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios from "axios";
-import "./Auth.css";
 
 const Auth = () => {
 
--- src/Components/Pages/Success.jsx
+++ src/Components/Pages/Success.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const Success = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1 style={{ color: "green" }}>🎉 Payment Successful!</h1>
-      <p>Your order has been placed successfully.</p>
-      <button onClick={() => navigate("/")}>
-        Go To Home
-      </button>
+    <div className="success-container">
+      <div className="success-card">
+        <h1> Payment Successful!</h1>
+        <p>Your order has been placed successfully.</p>
+        <button onClick={() => navigate("/")}>
+          Go To Home
+        </button>
+      </div>
     </div>
   );
 };
